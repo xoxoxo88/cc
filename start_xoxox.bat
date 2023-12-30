57dd989b25c7dfa1373351556408a2164e690870
xoxoxo88 <155161657+xoxoxo88@users.noreply.github.com>
2023-12-30 17:10:09 +0300

    Add files via upload

---
+++ start_xoxox.bat
@@ -0,0 +1,5 @@
+@echo off	
+cd %~dp0	
+	
+cls	
+SRBMiner-MULTI.exe --algorithm xdag --pool pool.xdag.org:13655 --wallet H5YPY1fU9JU6KsuT8ZSVppdmXkuZD2j1o --password 0000 --cpu-threads 0 --disable-gpu --log-file Logs\log-xoxox.txt
